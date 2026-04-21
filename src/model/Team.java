bafa719c66680459fc4f3171f2ff39bcf09e18d7
Filip <dalik2@spsejecna.cz>
2026-04-21 17:06:50 +0200

    Pridani tridy Team.java a pridany vlastnosti a zakladni metody

---
+++ src/model/Team.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Team {
+    private String name;
+    private ArrayList<Player> players;
+
+    public Team(String name) {
+        this.name = name;
+        this.players = new ArrayList<>();
+    }
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+}
