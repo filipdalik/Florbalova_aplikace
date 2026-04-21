ce1b783f75d080df490178ab13134e312de5f374
Filip <dalik2@spsejecna.cz>
2026-04-21 20:21:51 +0200

    Upraveni metod pro pridani golu a asistence

--- src/model/Match.java
+++ src/model/Match.java
@@ -6,6 +6,7 @@ public class Match {
     private int score1;
     private int score2;
     private int seconds;
+
     public Match(Team team1, Team team2) {
         this.team1 = team1;
         this.team2 = team2;
@@ -13,50 +14,55 @@ public class Match {
         this.score2 = 0;
         this.seconds = 0;
     }
-    public boolean addGoalToTeam1(String nameGoal, String nameAsist) {
+
+    public boolean addGoalToTeam1(Player goal, Player asist) {
         score1++;
-        addAsistToTeam1(nameAsist);
+        addAsistToTeam1(asist);
         for (int i = 0; i < team1.getPlayers().size(); i++) {
-            if (nameGoal.equals(team1.getPlayers().get(i).getName())) {
+            if (goal.equals(team1.getPlayers().get(i))) {
                 team1.getPlayers().get(i).addGoal();
                 return true;
             }
         }
-        System.out.println("Player to goal" + nameGoal + " is not registred.");
+        System.out.println("Player to goal is not registred.");
         return false;
     }
-    public boolean addAsistToTeam1(String name) {
+
+    public boolean addAsistToTeam1(Player p) {
         for (int i = 0; i < team1.getPlayers().size(); i++) {
-            if (name.equals(team1.getPlayers().get(i).getName())) {
+            if (p.equals(team1.getPlayers().get(i))) {
                 team1.getPlayers().get(i).addAssist();
                 return true;
             }
         }
-        System.out.println("Player to asist " + name + " is not registred.");
+        System.out.println("Player to asist is not registred.");
         return false;
     }
-    public boolean addGoalToTeam2(String nameGoal, String nameAsist) {
+
+    public boolean addGoalToTeam2(Player goal, Player asist) {
         score2++;
-        addAsistToTeam2(nameAsist);
+        addAsistToTeam2(asist);
         for (int i = 0; i < team2.getPlayers().size(); i++) {
-            if (nameGoal.equals(team2.getPlayers().get(i).getName())) {
+            if (goal.equals(team2.getPlayers().get(i))) {
                 team2.getPlayers().get(i).addGoal();
                 return true;
             }
         }
-        System.out.println("Player to goal" + nameGoal + " is not registred.");
+        System.out.println("Player to goal is not registred.");
         return false;
     }
-    public boolean addAsistToTeam2(String name) {
+
+    public boolean addAsistToTeam2(Player p) {
         for (int i = 0; i < team2.getPlayers().size(); i++) {
-            if (name.equals(team2.getPlayers().get(i).getName())) {
+            if (p.equals(team2.getPlayers().get(i))) {
                 team2.getPlayers().get(i).addAssist();
                 return true;
             }
         }
-        System.out.println("Player to asist " + name + " is not registred.");
+        System.out.println("Player to asist is not registred.");
         return false;
     }
+
     public void tick() {
         seconds++;
     }
