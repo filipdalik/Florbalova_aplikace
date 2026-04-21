e07d8703f903fd9a1d1d97a3e4f0c9bf3f73ac55
Filip <dalik2@spsejecna.cz>
2026-04-21 17:01:58 +0200

    Pridani vlastnosti ve tride Player.java

--- src/model/Player.java
+++ src/model/Player.java
@@ -3,14 +3,49 @@ package model;
 public class Player {
     private String name;
     private int number;
+    private int goals;
+    private int assists;
+    private int points;
 
     public Player(String name, int number) {
         this.name = name;
         this.number = number;
+        this.goals = 0;
+        this.assists = 0;
+        this.points = 0;
+    }
+
+    public void addGoals(){
+        goals++;
+        points++;
+    }
+    public void addAssists(){
+        assists++;
+        points++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public int getPoints() {
+        return points;
     }
 
     @Override
     public String toString() {
-        return number + " - " + name;
+        return number + " - " + name + " (G:" + goals + ", A:" + assists + ")";
     }
 }
