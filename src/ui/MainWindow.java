6aa60bafa3a39e443d48e44be1d43a170d293d55
Filip <dalik2@spsejecna.cz>
2026-04-21 20:47:12 +0200

    Pridani tridy MainWindow, ve ktere bude probihat veskera cast aplikace a pujde v ni pridavat goly/asistence/tresty a pujde v ni otvirat i okno s poznamkami.

---
+++ src/ui/MainWindow.java
@@ -0,0 +1,19 @@
+package ui;
+
+import model.Match;
+import model.Player;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+    private JLabel scoreLabel;
+    private JLabel timeLabel;
+    private Timer timer;
+    private boolean running = true;
+    private Match match;
+
+    public MainWindow(Match match) {
+
+    }
+}
