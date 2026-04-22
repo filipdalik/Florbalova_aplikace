80e98dfa1a64d970e113c7990db50d7768d9dd98
Filip <dalik2@spsejecna.cz>
2026-04-22 19:49:01 +0200

    Vytvoreni konstruktoru u EvantsWindow a udelani okna na poznamky/zaznam ze zapasu

--- src/ui/EventsWindow.java
+++ src/ui/EventsWindow.java
@@ -1,4 +1,27 @@
 package ui;
 
-public class EventsWindow {
+import model.Match;
+
+import javax.swing.*;
+
+public class EventsWindow extends JFrame {
+    public EventsWindow(Match match) {
+        setTitle("EVENT");
+        setSize(400, 1000);
+        setLocationRelativeTo(null);
+        JTextArea area = new JTextArea();
+        area.setEditable(false);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < match.getEvents().size(); i++) {
+            sb.append(match.getEvents().get(i)).append("\n");
+        }
+
+        area.setText(sb.toString());
+
+        add(new JScrollPane(area));
+
+        setVisible(true);
+    }
 }
