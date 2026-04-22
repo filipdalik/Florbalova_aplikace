2683f1a90bbf2b23ddab0747243836a58f399b37
Filip <dalik2@spsejecna.cz>
2026-04-22 14:29:12 +0200

    Upraveni pridani golu ve tride Match.java a metody tick()

--- src/model/Match.java
+++ src/model/Match.java
@@ -1,11 +1,14 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Match {
     private Team team1;
     private Team team2;
     private int score1;
     private int score2;
     private int seconds;
+    private ArrayList<Event> events;
 
     public Match(Team team1, Team team2) {
         this.team1 = team1;
@@ -13,66 +16,66 @@ public class Match {
         this.score1 = 0;
         this.score2 = 0;
         this.seconds = 0;
+        this.events = new ArrayList<>();
+    }
+    public boolean addGoal(Team team, Player goal, Player assist){
+        if (goal == null){
+            return false;
+        }
+        if (team == team1){
+            score1++;
+        }else if (team == team2){
+            score2++;
+        }
+        goal.addGoal();
+
+        if (assist != null){
+            assist.addAssist();
+        }
+
+        String time = getMinutes() + ":" + String.format("%02d", getSeconds());
+        String text = goal.getName() + "scored a goal";
+        if (assist != null){
+            text += ", assisted " + assist.getName()+".";
+        }
+        text += " without assistance.";
+        events.add(new Event(time, text));
+        return true;
     }
 
-    public boolean addGoalToTeam1(Player goal, Player asist) {
-        score1++;
-        addAsistToTeam1(asist);
-        for (int i = 0; i < team1.getPlayers().size(); i++) {
-            if (goal.equals(team1.getPlayers().get(i))) {
-                team1.getPlayers().get(i).addGoal();
-                return true;
-            }
+    public void addPenalty(Player player, int seconds, String type) {
+        if (player == null){
+            return;
         }
-        System.out.println("Player to goal is not registred.");
-        return false;
+
+        player.addPenalty(seconds);
+        String time = getMinutes() + ":" + String.format("%02d", getSeconds());
+        events.add(new Event(time, player.getName() + " - PENALTY: " + type));
     }
 
-    public boolean addAsistToTeam1(Player p) {
-        for (int i = 0; i < team1.getPlayers().size(); i++) {
-            if (p.equals(team1.getPlayers().get(i))) {
-                team1.getPlayers().get(i).addAssist();
-                return true;
-            }
-        }
-        System.out.println("Player to asist is not registred.");
-        return false;
-    }
-
-    public boolean addGoalToTeam2(Player goal, Player asist) {
-        score2++;
-        addAsistToTeam2(asist);
-        for (int i = 0; i < team2.getPlayers().size(); i++) {
-            if (goal.equals(team2.getPlayers().get(i))) {
-                team2.getPlayers().get(i).addGoal();
-                return true;
-            }
-        }
-        System.out.println("Player to goal is not registred.");
-        return false;
-    }
-
-    public boolean addAsistToTeam2(Player p) {
-        for (int i = 0; i < team2.getPlayers().size(); i++) {
-            if (p.equals(team2.getPlayers().get(i))) {
-                team2.getPlayers().get(i).addAssist();
-                return true;
-            }
-        }
-        System.out.println("Player to asist is not registred.");
-        return false;
+    public void addEvent(String text) {
+        String time = getMinutes() + ":" + String.format("%02d", getSeconds());
+        events.add(new Event(time, text));
     }
 
     public void tick() {
         seconds++;
+        for (int i = 0; i < team1.getPlayers().size(); i++) {
+            Player p = team1.getPlayers().get(i);
+            p.tickPenalty();
+        }
+        for (int i = 0; i < team2.getPlayers().size(); i++) {
+            Player p = team2.getPlayers().get(i);
+            p.tickPenalty();
+        }
     }
 
     public int getMinutes() {
-        return seconds/60;
+        return seconds / 60;
     }
 
     public int getSeconds() {
-        return seconds%60;
+        return seconds % 60;
     }
 
     public int getScore1() {
@@ -89,5 +92,8 @@ public class Match {
 
     public Team getTeam2() {
         return team2;
+    }
+    public ArrayList<Event> getEvents() {
+        return events;
     }
 }
