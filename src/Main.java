bee573ec529b713142459e2c52126e0e677ae651
dalik2 <dalik2@spsejecna.cz>
2026-04-27 13:35:50 +0200

    Predelani tridy WelcomeWindow.java a pridani novych tlacitek

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,26 @@ import ui.WelcomeWindow;
 
 public class Main {
     public static void main(String[] args) {
-        Player player1 = new Player("Adam Cerny",58);
-        Player player2 = new Player("Filip Turek",5);
-        Player player3 = new Player("Andrej Novak",71);
-        Player player4 = new Player("Daniel Novy",7);
 
-        Team team1 = new Team("FBC Praha");
-        team1.addPlayer(player1);
-        team1.addPlayer(player2);
+        WelcomeWindow welcomeWindow = new WelcomeWindow();
 
-        Team team2 = new Team("FBC Praha");
-        team2.addPlayer(player3);
-        team2.addPlayer(player4);
 
-        Match match = new Match(team1,team2);
-        MainWindow mainWindow = new MainWindow(match);
+
+//        Player player1 = new Player("Adam Cerny",58);
+//        Player player2 = new Player("Filip Turek",5);
+//        Player player3 = new Player("Andrej Novak",71);
+//        Player player4 = new Player("Daniel Novy",7);
+//
+//        Team team1 = new Team("FBC Praha");
+//        team1.addPlayer(player1);
+//        team1.addPlayer(player2);
+//
+//        Team team2 = new Team("FBC Praha");
+//        team2.addPlayer(player3);
+//        team2.addPlayer(player4);
+//
+//        Match match = new Match(team1,team2);
+//        MainWindow mainWindow = new MainWindow(match);
 
 
     }
--- src/ui/WelcomeWindow.java
+++ src/ui/WelcomeWindow.java
@@ -2,28 +2,77 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
 
 public class WelcomeWindow extends JFrame {
 
     public WelcomeWindow() {
         setTitle("Floorball app");
-        setSize(750, 500);
+        setSize(850, 650);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
-        JLabel label = new JLabel("Floorball App", SwingConstants.CENTER);
-        label.setFont(new Font("Segoe UI", Font.BOLD, 480));
 
-        JButton start = new JButton("Start");
-        start.addActionListener(e->{
+
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(43, 42, 42));
+        panel.setLayout(new BorderLayout());
+
+
+        JLabel title = new JLabel("FLOORBALL APP", JLabel.CENTER);
+        title.setForeground(Color.WHITE);
+        title.setFont(new Font("Times New Roma", Font.BOLD, 60));
+        title.setBorder(BorderFactory.createEmptyBorder(40,0,20,0));
+        panel.add(title,BorderLayout.NORTH);
+
+        JPanel panel1 = new JPanel();
+        panel1.setBackground(new Color(43, 42, 42));
+        panel1.setLayout(new GridBagLayout());
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBackground(new Color(43, 42, 42));
+        buttonPanel.setLayout(new GridLayout(3,1,0,20));
+
+
+        JButton startButton = new JButton("START");
+        startButton.setFocusPainted(false);
+        startButton.setForeground(Color.WHITE);
+        startButton.setFont(new Font("Segoe UI",Font.BOLD,25));
+        startButton.setBackground(new Color(234, 141, 0));
+        startButton.setBorder(BorderFactory.createEmptyBorder());
+        startButton.setPreferredSize(new Dimension(200,50));
+
+
+        JButton settingsButton = new JButton("SETTINGS");
+        settingsButton.setFocusPainted(false);
+        settingsButton.setForeground(Color.WHITE);
+        settingsButton.setFont(new Font("Segoe UI",Font.BOLD,25));
+        settingsButton.setBackground(new Color(120, 0, 0));
+        settingsButton.setBorder(BorderFactory.createEmptyBorder());
+        settingsButton.setPreferredSize(new Dimension(200,50));
+
+        JButton endButton = new JButton("END");
+        endButton.setFocusPainted(false);
+        endButton.setForeground(Color.WHITE);
+        endButton.setFont(new Font("Segoe UI",Font.BOLD,25));
+        endButton.setBackground(new Color(18, 1, 158));
+        endButton.setBorder(BorderFactory.createEmptyBorder());
+        endButton.setPreferredSize(new Dimension(200,50));
+
+        buttonPanel.add(startButton);
+        buttonPanel.add(settingsButton);
+        buttonPanel.add(endButton);
+        panel1.add(buttonPanel);
+        panel.add(panel1,BorderLayout.CENTER);
+
+        startButton.addActionListener(e->{
             new StartWindow();
         });
 
-        setLayout(new BorderLayout());
-        add(label, BorderLayout.CENTER);
-        add(start, BorderLayout.SOUTH);
+        endButton.addActionListener(e->{
+            System.exit(0);
+        });
 
+        add(panel);
         setVisible(true);
     }
 }
