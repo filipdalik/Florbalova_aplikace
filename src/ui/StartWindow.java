1fd61e4bda880a8a56957d925b34d596bd21ac7b
Filip <dalik2@spsejecna.cz>
2026-04-29 10:51:58 +0200

    Pridani funkcnosti tlacitek a pridani tlacitek a panelu na okno

--- src/ui/StartWindow.java
+++ src/ui/StartWindow.java
@@ -31,6 +31,57 @@ public class StartWindow extends JFrame {
         JButton edit = new JButton("Edit team");
         JButton start = new JButton("Start match");
 
+        create.addActionListener(e -> {
+            String name = JOptionPane.showInputDialog("Team name:");
+            if (name == null || name.isEmpty()){
+                return;
+            }
+            Team t = new Team(name);
+            manager.addTeam(t);
+            team1Box.addItem(t);
+            team2Box.addItem(t);
+        });
+
+        remove.addActionListener(e -> {
+            String name = JOptionPane.showInputDialog("Team name:");
+            if (name == null || name.isEmpty()){
+                return;
+            }
+            manager.removeTeam(name);
+        });
+
+        edit.addActionListener(e -> {
+            Team t = (Team) team1Box.getSelectedItem();
+            if (t != null) {
+                new TeamEditorWindow(manager, t);
+            }
+        });
+
+        start.addActionListener(e -> {
+            Team t1 = (Team) team1Box.getSelectedItem();
+            Team t2 = (Team) team2Box.getSelectedItem();
+            if (t1 == null || t2 == null){
+                return;
+            }
+            if (t1 == t2){
+                JOptionPane.showMessageDialog(this, "You can't choose same teams!");
+                return;
+            }
+            Match match = new Match(copy(t1), copy(t2));
+            new MainWindow(match);
+            dispose();
+        });
+
+        add(team1Box);
+        add(team2Box);
+        JPanel panel = new JPanel(new BorderLayout(100,100));
+        panel.add(create, BorderLayout.EAST);
+        panel.add(remove, BorderLayout.WEST);
+        add(panel);
+        add(edit);
+        add(start);
+
+        setVisible(true);
     }
 
     private void loadTeams() {
@@ -42,11 +93,9 @@ public class StartWindow extends JFrame {
 
     private Team copy(Team original) {
         Team c = new Team(original.getName());
-
         for (Player p : original.getPlayers()) {
             c.addPlayer(new Player(p.getName(), p.getNumber()));
         }
-
         return c;
     }
 }
