8bf6d913bb9b43d8e23057c5b4fc48f4e82c7e09
Filip <dalik2@spsejecna.cz>
2026-04-29 10:58:27 +0200

    Vytvoreni tridy TeamEditorWindow, kde se bude spravovat dany team a pujde do nej pridavat novy hrac a nebo i odebirat hrace

---
+++ src/ui/TeamEditorWindow.java
@@ -0,0 +1,16 @@
+package ui;
+
+import model.*;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TeamEditorWindow extends JFrame {
+
+    private Team team;
+    private TeamManager manager;
+
+    public TeamEditorWindow(TeamManager manager, Team team) {
+
+    }
+}
