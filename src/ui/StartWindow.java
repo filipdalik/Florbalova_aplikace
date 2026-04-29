6c973d5af8f139455a8687058f5573b3fc265865
Filip <dalik2@spsejecna.cz>
2026-04-29 10:43:19 +0200

    Pridani vlastnosti a konstruktoru ve tride StartWindow.java

--- src/ui/StartWindow.java
+++ src/ui/StartWindow.java
@@ -1,10 +1,18 @@
 package ui;
 
+import model.*;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class StartWindow extends JFrame {
 
-    public StartWindow(){
+    private TeamManager manager;
+
+    private JComboBox<Team> team1Box;
+    private JComboBox<Team> team2Box;
+
+    public StartWindow() {
 
     }
 }
