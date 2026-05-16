85e7ff6852ec854640e5b456f55e1092beea92cb
Filip <dalik2@spsejecna.cz>
2026-05-16 11:30:51 +0200

    Pridani metody pro konec zapasu - handleFinalEnd a pak upraveni stopMatch a timeru a uprava vzhledu tlacitek

--- src/ui/MainWindow.java
+++ src/ui/MainWindow.java
@@ -12,6 +12,7 @@ public class MainWindow extends JFrame {
     private JLabel timeLabel;
     private JLabel team1Label;
     private JLabel team2Label;
+    private JLabel periodLabel;
 
     private Timer timer;
     private boolean running = false;
@@ -21,19 +22,25 @@ public class MainWindow extends JFrame {
 
         this.match = match;
 
+
         setTitle("Match");
         setSize(950, 750);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
+        setLayout(new BorderLayout());
 
-        scoreLabel = new JLabel("0 : 0", SwingConstants.CENTER);
-        scoreLabel.setFont(new Font("Arial", Font.BOLD, 120));
-
+        JPanel northPanel = new JPanel(new GridLayout(2, 1));
         timeLabel = new JLabel("00:00", SwingConstants.CENTER);
         timeLabel.setFont(new Font("Arial", Font.BOLD, 80));
         timeLabel.setForeground(new Color(108, 0, 0));
+        periodLabel = new JLabel("1. period", SwingConstants.CENTER);
+        periodLabel.setFont(new Font("Arial", Font.ITALIC, 25));
+        northPanel.add(timeLabel);
+        northPanel.add(periodLabel);
 
+        scoreLabel = new JLabel("0 : 0", SwingConstants.CENTER);
+        scoreLabel.setFont(new Font("Arial", Font.BOLD, 120));
 
         team1Label = new JLabel(match.getTeam1().getName(), SwingConstants.RIGHT);
         team2Label = new JLabel(match.getTeam2().getName(), SwingConstants.LEFT);
@@ -42,6 +49,15 @@ public class MainWindow extends JFrame {
         team1Label.setForeground(new Color(23, 50, 106));
         team2Label.setForeground(new Color(23, 50, 106));
 
+        JPanel teamPanel = new JPanel(new GridLayout(1, 2, 50, 0));
+        teamPanel.add(team1Label);
+        teamPanel.add(team2Label);
+
+        JPanel centerPanel = new JPanel(new BorderLayout());
+        centerPanel.add(scoreLabel, BorderLayout.CENTER);
+        centerPanel.add(teamPanel, BorderLayout.SOUTH);
+
+        JPanel buttonPanel = new JPanel(new FlowLayout());
         JButton goal1Button = new JButton("GOAL TEAM 1");
         JButton goal2Button = new JButton("GOAL TEAM 2");
         JButton stopButton = new JButton("START");
@@ -51,20 +67,6 @@ public class MainWindow extends JFrame {
         JButton events = new JButton("EVENTS");
         JButton addEvent = new JButton("ADD EVENT");
 
-        JPanel buttons = new JPanel();
-        buttons.add(goal1Button);
-        buttons.add(goal2Button);
-        buttons.add(stopButton);
-        buttons.add(penalty);
-        buttons.add(events);
-        buttons.add(addEvent);
-
-        timer = new Timer(1000, e -> {
-            if (running) {
-                match.tick();
-                updateUIData();
-            }
-        });
 
         goal1Button.addActionListener(e -> {
 
@@ -108,7 +110,7 @@ public class MainWindow extends JFrame {
 
             Player assist = (Player) JOptionPane.showInputDialog(
                     this,
-                    "Assist (optional):",
+                    "Assist (cancel = no assist):",
                     "Assist team 2",
                     JOptionPane.PLAIN_MESSAGE,
                     null,
@@ -140,28 +142,60 @@ public class MainWindow extends JFrame {
             }
         });
 
-        setLayout(new BorderLayout());
-        add(timeLabel, BorderLayout.NORTH);
-        JPanel center = new JPanel(new BorderLayout());
-        center.add(scoreLabel, BorderLayout.CENTER);
-        JPanel teamPanel = new JPanel(new BorderLayout());
-        teamPanel.add(team2Label, BorderLayout.EAST);
-        teamPanel.add(team1Label, BorderLayout.WEST);
+        buttonPanel.add(goal1Button);
+        buttonPanel.add(goal2Button);
+        buttonPanel.add(penalty);
+        buttonPanel.add(stopButton);
+        buttonPanel.add(events);
+        buttonPanel.add(addEvent);
 
-        center.add(teamPanel, BorderLayout.SOUTH);
-        teamPanel.setBorder(BorderFactory.createEmptyBorder(0, 100, 350, 100));
-        add(center, BorderLayout.CENTER);
-        add(buttons, BorderLayout.SOUTH);
 
+        add(northPanel, BorderLayout.NORTH);
+        add(centerPanel, BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.SOUTH);
         setVisible(true);
+
+        timer = new Timer(50, e -> {
+            if (running) {
+                boolean periodEnd = match.tick();
+                updateUIData();
+                if (periodEnd || match.getMinutes() >= 20) {
+                    stopMatch();
+                    stopButton.setText("START");
+                }
+            }
+        });
     }
 
     private void updateUIData() {
-
+        timeLabel.setText(String.format("%02d:%02d", match.getMinutes(), match.getSeconds()));
         scoreLabel.setText(match.getScore1() + " : " + match.getScore2());
+        periodLabel.setText(match.getPeriod() + ". period");
+    }
 
-        timeLabel.setText(String.format("%02d:%02d",
-                match.getMinutes(),
-                match.getSeconds()));
+    private void stopMatch() {
+        running = false;
+        timer.stop();
+
+        if (match.getPeriod() < 3){
+            JOptionPane.showMessageDialog(this, "End of " + match.getPeriod() + ". period!");
+            match.nextPeriod();
+            updateUIData();
+        } else {
+            handleFinalEnd();
+        }
+    }
+    private void handleFinalEnd() {
+        match.saveToHistory();
+        String text = "The match has ended! \n Score: " + match.getScore1() + " : " + match.getScore2() + "\n You can see this data in history.";
+        Object[] options = {"Back to menu", "End"};
+        int n = JOptionPane.showOptionDialog(this, text, "End of match", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+
+        if (n == JOptionPane.YES_OPTION) {
+            new WelcomeWindow();
+            dispose();
+        } else {
+            System.exit(0);
+        }
     }
 }
