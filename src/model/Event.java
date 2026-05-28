caac8db16d27a901cf92e0c1f923fefc13511a99
Filip <dalik2@spsejecna.cz>
2026-05-28 07:36:30 +0200

    Doplnění Java docs do tridy Event.java

--- src/model/Event.java
+++ src/model/Event.java
@@ -1,22 +1,40 @@
 package model;
-
+/**
+ * Tato třída reprezentuje konkrétní událost v zápase.
+ * Pro tuto událost je důležitý čas a textový popis události
+ */
 public class Event {
     private String time;
     private String text;
 
+    /**
+     * Konstruktor pro vytvoření nové instance události.
+     */
     public Event(String time, String text) {
         this.time = time;
         this.text = text;
     }
 
+    /**
+     * Vrátí čas, kdy k události došlo.
+     * @return String time
+     */
     public String getTime() {
         return time;
     }
 
+    /**
+     * Vrátí textový popis události.
+     * @return String text.
+     */
     public String getText() {
         return text;
     }
 
+    /**
+     * Vrátí text, ve kterém je vše potřebné pro danou událost.
+     * @return String text.
+     */
     @Override
     public String toString() {
         return time + " -> " + text;
